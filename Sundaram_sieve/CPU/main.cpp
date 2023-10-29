b5be32669e7af03c21aad8969feb7549f9fd9ccb
P0rtier <patrykw595@gmail.com>
2023-10-29 18:12:05 +0100

    init sundaram

--- Sundaram_sieve/CPU/main.cpp
+++ Sundaram_sieve/CPU/main.cpp
@@ -2,26 +2,37 @@
 #include <thread>
 #include <vector>
 #include <chrono>
+#include <mutex>
 
-// A dummy function
-void foo(int params, int params2)
-{
-  for (int i = 0; i < params; i++)
-  {
-    std::cout << "Thread using function"
-                 " pointer as callable\n";
-  }
+const int N = 100;
+
+std::vector<bool> is_prime(N + 1, true);
+std::mutex mtx;
+
+void mark_multiples(int p) {
+    for (int i = p * p; i <= N; i += p) {
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            is_prime[i] = false;
+        }
+    }
+}
+
+void sieve_of_eratosthenes(int start, int end) {
+    for (int p = 2; p * p <= end; p++) {
+        if (is_prime[p]) {
+            mark_multiples(p);
+        }
+    }
 }
 
 void runThreads(int threadsNumber)
 {
-  int fooParams = 3;
-  int fooParams2 = 2;
-
   std::vector<std::thread> threads;
-  for (int i = 0; i < threadsNumber; ++i)
-  {
-    threads.push_back(std::thread(foo, fooParams, fooParams2));
+  for (int i = 0; i < threadsNumber; i++) {
+      int start = (i * N) / threadsNumber;
+      int end = ((i + 1) * N) / threadsNumber - 1;
+      threads.emplace_back(sieve_of_eratosthenes, start, end);
   }
 
   for (auto &th : threads)
@@ -32,16 +43,24 @@ void runThreads(int threadsNumber)
 
 int main()
 {
+  // int threadsNumber = std::thread::hardware_concurrency();
   int threadsNumber = 10;
 
   auto start = std::chrono::high_resolution_clock::now();
   runThreads(threadsNumber);
   auto end = std::chrono::high_resolution_clock::now();
 
-  auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
   std::cout << "Time taken by threads: "
-            << duration.count() << " nanoseconds" << std::endl;
+            << duration.count() << " microseconds" << std::endl;
+
+  // print primes
+  for (int p = 2; p <= N; p++) {
+      if (is_prime[p]) {
+          std::cout << p << " ";
+      }
+  }
 
   return 0;
 }
