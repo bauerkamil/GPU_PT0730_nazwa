0dd3ac465b5b0d541dbb08a892e895567eccc67f
P0rtier <patrykw595@gmail.com>
2023-10-29 18:18:51 +0100

    sundaram multi-thread

--- Sundaram_sieve/CPU/main.cpp
+++ Sundaram_sieve/CPU/main.cpp
@@ -4,40 +4,41 @@
 #include <chrono>
 #include <mutex>
 
-const int N = 100;
+const int N = 10000;
 
-std::vector<bool> is_prime(N + 1, true);
+std::vector<bool> is_prime((N - 1) / 2 + 1, true);
 std::mutex mtx;
 
 void mark_multiples(int p) {
-    for (int i = p * p; i <= N; i += p) {
-        {
-            std::lock_guard<std::mutex> lock(mtx);
-            is_prime[i] = false;
-        }
+  for (int i = 2 * p; i <= N; i += p) {
+    {
+      std::lock_guard<std::mutex> lock(mtx);
+      is_prime[(i - 1) / 2] = false;
     }
+  }
 }
 
-void sieve_of_eratosthenes(int start, int end) {
-    for (int p = 2; p * p <= end; p++) {
-        if (is_prime[p]) {
-            mark_multiples(p);
-        }
+void sieve_of_sundaram(int start, int end) {
+  for (int i = 1; 2 * i + 1 <= end; i++) {
+    if (is_prime[i]) {
+      int p = 2 * i + 1;
+      mark_multiples(p);
     }
+  }
 }
 
 void runThreads(int threadsNumber)
 {
   std::vector<std::thread> threads;
   for (int i = 0; i < threadsNumber; i++) {
-      int start = (i * N) / threadsNumber;
-      int end = ((i + 1) * N) / threadsNumber - 1;
-      threads.emplace_back(sieve_of_eratosthenes, start, end);
+    int start = ((i * N) / threadsNumber + 1) / 2;
+    int end = (((i + 1) * N) / threadsNumber) / 2;
+    threads.emplace_back(sieve_of_sundaram, start, end);
   }
 
   for (auto &th : threads)
   {
-    th.join();
+  th.join();
   }
 }
 
@@ -53,13 +54,13 @@ int main()
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
   std::cout << "Time taken by threads: "
-            << duration.count() << " microseconds" << std::endl;
+      << duration.count() << " microseconds" << std::endl;
 
-  // print primes
-  for (int p = 2; p <= N; p++) {
-      if (is_prime[p]) {
-          std::cout << p << " ";
-      }
+  std::cout << "2 ";
+  for (int i = 1; 2 * i + 1 <= N; i++) {
+    if (is_prime[i]) {
+      std::cout << 2 * i + 1 << " ";
+    }
   }
 
   return 0;
