8b37be40c26bd6038efe4534f40cad835a9e713e
Kamil Bauer <259102@student.pwr.edu.pl>
2023-11-29 19:03:40 +0100

    fix range in sundaram cpu

--- Sundaram_sieve/CPU/main.cpp
+++ Sundaram_sieve/CPU/main.cpp
@@ -3,22 +3,22 @@
 #include <thread>
 #include <chrono>
 
-const int N = 100000;
-const int K = (N - 2) / 2;
+const uint64_t N = 1000000;
+const uint64_t K = (N - 2) / 2;
 // std::vector<bool> is_prime(K, true);
 bool *is_prime;
 
-void sieve_of_sundaram(int start, int end)
+void sieve_of_sundaram(uint64_t start, uint64_t end)
 {
-    for (long i = start; i <= end; i++)
+    for (uint64_t i = start; i <= end; i++)
     {
         if (is_prime[i])
         {
-            long j = i;
-            long val = i + j + 2 * i * j;
+            uint64_t j = i;
+            uint64_t val = i + j + 2 * i * j;
             while (val <= K && val > 0)
             {
-                int index = i + j + 2 * i * j;
+                uint64_t index = i + j + 2 * i * j;
                 is_prime[index] = false;
                 j += 1;
                 val = i + j + 2 * i * j;
@@ -30,12 +30,12 @@ void sieve_of_sundaram(int start, int end)
 void print_primes()
 {
     int count = 1;
-    std::cout << "Primes: 2 ";
-    for (int p = 1; p <= K; p++)
+    // std::cout << "Primes: 2 ";
+    for (uint64_t p = 1; p <= K; p++)
     {
         if (is_prime[p])
         {
-            std::cout << 2 * p + 1 << " ";
+            // std::cout << 2 * p + 1 << " ";
             count++;
         }
     }
@@ -56,8 +56,8 @@ void run_threads(int threads_number, bool print_results = false)
     std::vector<std::thread> threads;
     for (int i = 0; i < threads_number; i++)
     {
-        int start = i * chunk_size + 1;
-        int end = (i == threads_number - 1) ? K : (i + 1) * chunk_size;
+        uint64_t start = i * chunk_size + 1;
+        uint64_t end = (i == threads_number - 1) ? K : (i + 1) * chunk_size;
         threads.emplace_back(sieve_of_sundaram, start, end);
     }
 
@@ -79,7 +79,7 @@ int main()
     std::cout << "Target num: " << N << std::endl;
     std::cout << "Threads: " << threads_number << std::endl;
     auto start = std::chrono::high_resolution_clock::now();
-    run_threads(threads_number);
+    run_threads(threads_number, true);
     auto end = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
